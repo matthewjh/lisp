2fa665ce48efdc618cf0217f5a4aced019ec31b3
Matthew Hill <matthew.jh@outlook.com>
2016-10-12 21:56:29 +0100

    solving more problems + added questions

--- 99problems.lisp
+++ 99problems.lisp
@@ -33,3 +33,36 @@
                  (f (cdr lst1) (cdr lst2) (equal (car lst1) (car lst2)))
                  acc)))
     (f lst (reverse lst) t)))
+
+(defun my-flatten1 (lst)
+  (let ((acc '()))
+    (labels ((f (lst)
+               (when lst
+                 (cond
+                   ((atom (car lst)) (setf acc (cons (car lst) acc))
+                    (f (cdr lst)))
+                   ((listp (car lst)) (f (car lst))
+                    (f (cdr lst)))
+                   (t (princ "Error"))))))
+      (f lst)
+      acc)))
+
+
+(defun my-flatten2 (lst)
+  (labels ((f (lst acc)
+             (if lst
+               (cond
+                 ((atom (car lst)) (f (cdr lst) (cons (car lst) acc)))
+                 ((listp (car lst)) (f (cdr lst) (f (car lst) acc))))
+               acc)))
+    (reverse (f lst nil))))
+             
+
+(defun compress (lst)
+  (labels ((f (lst prev-car acc)
+             (if lst
+                 (if (and prev-car (equal (car lst) prev-car))
+                     (f (cdr lst) (car lst) acc)
+                     (f (cdr lst) (car lst) (cons (car lst) acc)))
+                 acc)))
+    (reverse (f lst nil nil))))
